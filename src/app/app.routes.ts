c6ec2b1686cf556d2bbe865500b4ab45d4dab757
bintariq29 <hanzlat478@gmail.com>
2025-11-15 20:25:00 +0500

    login screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Signup } from './pages/signup/signup';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: Login
+    }, 
+    {
+        path: "login",
+        component: Signup
+    },
+    {
+        path: "signup",
+        component: Signup
+    }
+];
