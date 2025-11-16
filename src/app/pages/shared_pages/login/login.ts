3b2529400b31d771dfdb2eb5f3cb0430b43336c4
bintariq29 <hanzlat478@gmail.com>
2025-11-16 12:14:13 +0500

    login functionality works successfully

--- src/app/pages/shared_pages/login/login.ts
+++ src/app/pages/shared_pages/login/login.ts
@@ -1,16 +1,63 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
-  router=inject(Router)
-  onSignupTap(){
+  router = inject(Router)
+  username = "";
+  password = "";
+  http = inject(HttpClient)
+  onSignupTap() {
     this.router.navigateByUrl("/signup")
   }
+
+  onLoginTap() {
+    if (!this.username || this.username.trim() === "") {
+      alert("Username is empty!");
+      return;
+    }
+
+    if (!this.password || this.password.trim() === "") {
+      alert("Password is empty!");
+      return;
+    }
+
+    const url = "https://localhost:7212/api/User/" + this.username;
+    this.http.get<any>(url).subscribe({
+      next: (res) => {
+        console.log("API Response:", res);
+
+        // Check password
+        if (res.password === this.password) {
+          alert("Login Successful!");
+          if (res.userType == "admin") {
+            this.router.navigateByUrl("/admin_home")
+          }
+          if (res.userType == "student") {
+            this.router.navigateByUrl("/student_home")
+          }
+        } else {
+          alert("Incorrect Password!");
+          return;
+        }
+      },
+      error: (err) => {
+        console.error("API Error:", err);
+        alert(`User not found or server error! ${err}`);
+      }
+    });
+
+
+  }
+
+
+
 
 }
