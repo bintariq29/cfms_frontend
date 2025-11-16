1062ff7699c549a203d64fa04c6fff7fdedda056
bintariq29 <hanzlat478@gmail.com>
2025-11-16 11:29:06 +0500

    on signup navigations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/shared_pages/login/login';
 import { Signup } from './pages/shared_pages/signup/signup';
+import { AdminHome } from './pages/admin_pages/admin-home/admin-home';
+import { StudentHome } from './pages/student_pages/student-home/student-home';
+
 
 export const routes: Routes = [
     {
@@ -14,5 +17,13 @@ export const routes: Routes = [
     {
         path: "signup",
         component: Signup
-    }
+    },
+    {
+        path: "admin_home",
+        component: AdminHome
+    },
+    {
+        path: "student_home",
+        component: StudentHome
+    },
 ];
--- src/app/pages/shared_pages/signup/signup.ts
+++ src/app/pages/shared_pages/signup/signup.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../../../models/user_model';
+import { retry } from 'rxjs';
 
 @Component({
   selector: 'app-signup',
@@ -22,19 +23,49 @@ export class Signup {
   }
 
   signUpFunc() {
+    if (!this.username || this.username.trim() === "") {
+      alert("Username is empty!");
+      return;
+    }
+
+    if (!this.password || this.password.trim() === "") {
+      alert("Password is empty!");
+      return;
+    }
+
+    if (!this.confirmPassword || this.confirmPassword.trim() === "") {
+      alert("Confirm Password is empty!");
+      return;
+    }
+
+    if (this.password !== this.confirmPassword) {
+      alert("Passwords do not match!");
+      return;
+    }
+
+    if (!this.userType || this.userType.trim() === "") {
+      alert("UserType is required!");
+      return;
+    }
 
     const user: User = {
       userId: 0,
-      userName: "BinTariq",
-      password: "123",
-      userType: "admin"
+      userName: this.username,
+      password: this.password,
+      userType: this.userType
     };
 
 
     this.http.post("https://localhost:7212/api/User", user).subscribe({
       next: (res) => {
-        console.log("Response:", res);
-        alert("User registered successfully!");
+        debugger
+        if (this.userType == "admin") {
+          this.router.navigateByUrl("/admin_home")
+        }
+        if (this.userType == "student") {
+          this.router.navigateByUrl("/student_home")
+        }
+
       },
       error: (err) => {
         console.error("Error:", err);
