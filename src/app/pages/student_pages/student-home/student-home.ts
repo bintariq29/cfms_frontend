38956b489bbf33a30929e2e6efda4cd52c716c66
bintariq29 <hanzlat478@gmail.com>
2025-11-17 06:37:34 +0500

    student home panel

--- src/app/pages/student_pages/student-home/student-home.ts
+++ src/app/pages/student_pages/student-home/student-home.ts
@@ -1,18 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Course } from '../../../models/course_model';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-student-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './student-home.html',
   styleUrl: './student-home.css',
 })
 export class StudentHome implements OnInit {
   constructor(private route: ActivatedRoute) { }
   studentName: String = "";
+  allCourses: Course[] = [];
+  http = inject(HttpClient);
+  router = inject(Router)
+  getCourse() {
+    this.http.get<Course[]>('https://localhost:7212/GetCourses')
+      .subscribe({
+        next: (data) => {
+          debugger;
+          this.allCourses = data;
+          alert(`Courses loaded ${this.allCourses}`);
+          console.log('Courses loaded:', this.allCourses);
+        },
+        error: (err) => {
+          console.error('Error fetching courses:', err);
+        }
+      });
+  }
   ngOnInit(): void {
     this.studentName = String(this.route.snapshot.paramMap.get('name'));
+    this.getCourse();
   }
+
+  onShowFeedback(course: Course) {
+    debugger;
+    this.router.navigate(['/feedback', course.id]);
+  }
+
 
 
 }
